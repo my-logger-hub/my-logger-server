bba72866e7bb73269ac933a611406f036b3e8585
amigin <amigin@gmail.com>
2023-08-11 22:53:58 +0300

    Debugging

--- src/http/builder.rs
+++ src/http/builder.rs
@@ -1,20 +1,11 @@
 use std::sync::Arc;
 
-use my_http_server_controllers::controllers::{
-    ControllersAuthorization, ControllersMiddleware, RequiredClaims,
-};
+use my_http_server_controllers::controllers::ControllersMiddleware;
 
 use crate::app::AppContext;
 
 pub fn build_controllers(app: &Arc<AppContext>) -> ControllersMiddleware {
-    let mut result = ControllersMiddleware::new(
-        ControllersAuthorization::BearerAuthentication {
-            global: true,
-            global_claims: RequiredClaims::no_claims(),
-        }
-        .into(),
-        None,
-    );
+    let mut result = ControllersMiddleware::new(None, None);
 
     result.register_post_action(Arc::new(
         super::controllers::logs_income::PostLogsAction::new(app.clone()),
